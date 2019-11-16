6d1916d3e490eb187402f91f8b6b074acf5ee94f
Spicce <55270559+Spicce@users.noreply.github.com>
2019-11-16 15:29:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,33 @@
-makeCacheMatrix <- function(x = matrix()) { m <- NULL
-set <- function(y) {
-  x <<- y
-  m <<- NULL
-}
-get <- function() x
-setsolve <- function(solve) s<<-solve
-getsolve <- function() m
-list(set = set, get = get,
-     setsolve = setisolve,
-     getsolve = getsolve
+makeCacheMatrix <- function(x = matrix()) { 
+ set <- function(y) {
+   x <<- y
+  s <<- NULL
+ }
+ get <- function() x
+ setsolve <- function(solve) s<<-solve
+ getsolve <- function() s
+ list(set = set, get = get,
+      setsolve = setsolve,
+      getsolve = getsolve
 
-}
+ }
 
-#checking if the inverse exists if it does retrieve it
+ #checking if the inverse exists if it does retrieve it
 
-cacheSolve <- function(x, ...) {
-           m<NULL
-        ## Return a matrix that is the inverse of 'x'
-            m<-0
-            m<-get$inmatrix
-            if(!null m){'returning cache data'
-              }
-            return s}
-#if it is not solve then s=Null and solve it
-data <- x$get()
-s <- solve(data, ...)
-x$setsolve(s)
-s
-}
+ cacheSolveMatrix <- function(x, ...) {
+            s<NULL
+         ## Return a matrix that is the inverse of 'x'
+            s<-0
+             s<-get$solve
+             if(!null s){'returning cache data'
+               }
+             return s}
+ #if it is not solve then s=Null and solve it
+ data <- x$get()
+ s <- solve(data, ...)
+ x$setsolve(s)
+ s
+ }
 
+ }
 }
